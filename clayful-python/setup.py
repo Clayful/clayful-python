c8b9480b956c73130a2fe494afe3fcd42b096c0d
Daeik Kim <daeik.kim@clayful.io>
2020-03-03 14:10:16 +0900

    [Updated] `2.0.0` -> `2.0.1`

--- clayful-python/setup.py
+++ clayful-python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name                          = 'clayful',
-	version                       = '2.0.0',
+	version                       = '2.0.1',
 	description                   = 'Python SDK for Clayful API',
 	long_description              = open('README.md').read(),
 	long_description_content_type = 'text/markdown',
